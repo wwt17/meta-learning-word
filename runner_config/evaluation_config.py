23833b96f24385fc08317475646744e4370452e1
Wentao Wang <wentao.wang@nyu.edu>
2024-03-26 23:23:03 -0400

    evaluate all models

--- runner_config/evaluation_config.py
+++ runner_config/evaluation_config.py
@@ -3,11 +3,12 @@ grids = [
         "main_file": ["evaluation.py"],
         "data_dir": [r"word_use_data_clean/childes/word"],
         "pretrained_model": [
-            r"ckpt/meta-word_data_dir_word_use_data_clean:childes:word_config_gpt2_concat_False_context_length_128_no_new_token_False_n_examples_4_max_sample_times_0_batch_size_8_lr_0.0001_weight_decay_0.12_seed_0/best",
+            f"ckpt/meta-word_data_dir_word_use_data_clean:childes:word_config_gpt2_concat_False_context_length_128_no_new_token_False_n_examples_{n_examples}_max_sample_times_0_batch_size_8_lr_0.0001_weight_decay_0.12_seed_0/best",
         ],
-        "n_examples": [4],
+        "n_examples": [n_examples],
         "eval_n_classes": [(2, 3, 4, 5, 6)],
     }
+    for n_examples in [3, 4, 5, 6, 7, 8, 9, 10]
 ]
 # ordered flags to display in job name
 flags = [
