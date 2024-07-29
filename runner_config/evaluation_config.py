5e402b10c7588b9a34a216b825728702f06f3511
Wentao Wang <wentao.wang@nyu.edu>
2024-07-29 15:02:55 -0400

    update configs

--- runner_config/evaluation_config.py
+++ runner_config/evaluation_config.py
@@ -69,11 +69,55 @@ grids = [
             "prepend": [" "],
         }
     ],
+    [
+        {
+            "main_file": ["evaluation.py"],
+            "data_dir": [r"word_use_data/childes/word"],
+            "pretrained_model": [
+                f"ckpt/meta-word_pretrained_model_EleutherAI:pythia-160m_data_dir_word_use_data:childes:word_lm_False_embedding_init_mean_prompt__train_params_new_word_sep_n_examples_10_max_sample_times_0_batch_size_8_lr_0.0003_weight_decay_0.0_seed_0_eval_step_500/best",
+                f"ckpt/meta-word_pretrained_model_EleutherAI:pythia-160m_data_dir_word_use_data:childes:word_embedding_init_mean_train_params_new_word_sep_prompt_n_examples_10_max_sample_times_0_batch_size_8_lr_3e-05_weight_decay_0.0_seed_0_eval_step_500/best",
+            ][0:1],
+            "tokenizer": [r"EleutherAI/pythia-160m"],
+            "n_examples": list(range(10, 11)),
+            "eval_n_classes": [tuple(range(2, 11))],
+            "print_decoded_prefix": [True],
+            "new_word": ["<|new_word|>"],
+            "prompt": [
+                "",
+                "<|prompt_0|><|prompt_1|><|prompt_2|><|prompt_3|><|prompt_4|>",
+            ][0:1],
+            "sep": ["<|sep|>"],
+            "add_tokens": [
+                ("<|new_word|>", "<|sep|>"),
+                ("<|new_word|>", "<|sep|>", "<|prompt_0|>", "<|prompt_1|>", "<|prompt_2|>", "<|prompt_3|>", "<|prompt_4|>"),
+            ][0:1],
+        }
+    ],
+    [
+        {
+            "main_file": ["evaluation.py"],
+            "data_dir": [r"word_use_data/childes/word"],
+            "pretrained_model": [
+                f"ckpt/meta-word_pretrained_model_:scratch:ww2135:Meta-Llama-3-8B_data_dir_word_use_data:childes:word_lm_False_embedding_init_mean_prompt__train_params_new_word_sep_n_examples_10_max_sample_times_0_batch_size_8_lr_3e-05_weight_decay_0.0_seed_0_eval_step_500/best",
+                f"ckpt/meta-word_pretrained_model_:scratch:ww2135:Meta-Llama-3-8B_data_dir_word_use_data:childes:word_embedding_init_mean_train_params_new_word_sep_prompt_n_examples_10_max_sample_times_0_batch_size_8_lr_1e-05_weight_decay_0.0_seed_0/best",
+            ][1:2],
+            "tokenizer": [os.environ["SCRATCH"]+r"/Meta-Llama-3-8B"],
+            "n_examples": list(range(10, 11)),
+            "eval_n_classes": [tuple(range(2, 11))],
+            "print_decoded_prefix": [True],
+            "new_word": ["<|reserved_special_token_0|>"],
+            "prompt": [
+                "",
+                "<|reserved_special_token_2|><|reserved_special_token_3|><|reserved_special_token_4|><|reserved_special_token_5|><|reserved_special_token_6|>",
+            ][1:2],
+            "sep": ["<|reserved_special_token_1|>"],
+        }
+    ],
 ][-1]
 # ordered flags to display in job name
 flags = [
     "data_dir",
-    "pretrained_model",
-    "revision",
+    #"pretrained_model",
+    #"revision",
     "n_examples",
 ]
