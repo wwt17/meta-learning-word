b7d7c4320b8ea3077768200beb415b113ce1b950
Wentao Wang <wentao.wang@nyu.edu>
2024-11-30 23:43:42 -0500

    factor out functionalities of reading generations from output files into read_generations.py; fix the bug of not correctly reading the last output field (beam search outputs)

--- compare_generation.py
+++ compare_generation.py
@@ -6,6 +6,7 @@ from itertools import islice, combinations
 import json
 import numpy as np
 from utils import frac_repr
+from read_generations import input_fields, output_fields, fields, read_generations, extract_definition_from_generation
 
 
 use_outlines = False
@@ -31,92 +32,7 @@ class DelayedKeyboardInterrupt:
             self.old_handler(*self.signal_received)  # type: ignore
 
 
-input_fields = [
-    "ground-truth word",
-    "ground-truth prefix",
-    "     decoded prefix",
-    "ground-truth suffix",
-]
-output_fields = [
-    "greedy outputs",
-    "sample with top-p=0.92 outputs",
-    "beam search outputs",
-][:-1]
-fields = input_fields + output_fields
-
-
-def parse_example_with_generations(
-        lines: Iterable[str],
-        fields=fields,
-        cont_prefix_pattern="cont. {}: ".format,
-):
-    example = {}
-    last_lines = []
-    for line in lines:
-        field, sep, rest = line.partition(":")
-        if sep and field in fields:
-            last_lines = [rest]
-            example[field.lstrip()] = last_lines
-        else:
-            last_lines.append(line)
-    for field, value_lines in example.items():
-        if field.endswith("outputs"):
-            assert len(value_lines) >= 1
-            assert value_lines[0] == "\n"
-            value_lines.pop(0)
-            value = []
-            n_cont = 0
-            cont_prefix = cont_prefix_pattern(n_cont)
-            while value_lines:
-                assert value_lines[0].startswith(cont_prefix)
-                cont = value_lines.pop(0).removeprefix(cont_prefix)
-                n_cont += 1
-                cont_prefix = cont_prefix_pattern(n_cont)
-                while value_lines and not value_lines[0].startswith(cont_prefix):
-                    cont += value_lines.pop(0)
-                cont = cont.removesuffix("\n")
-                value.append(cont)
-        else:
-            value = "".join(value_lines)
-            value = value.removeprefix(" ")
-            value = value.removesuffix("\n")
-        example[field] = value
-    return example
-
-
-def read_generations(file, example_first_line_pattern="Example #{}:\n".format):
-    n_example = 0
-    example_first_line = example_first_line_pattern(n_example)
-    line = file.readline()
-    while True:
-        while line and line != example_first_line:
-            line = file.readline()
-        if not line:  # EOF
-            break
-        line = file.readline()
-        n_example += 1
-        example_first_line = example_first_line_pattern(n_example)
-        lines = []
-        while line and line != example_first_line:
-            lines.append(line)
-            line = file.readline()
-        if lines and lines[-1] == "\n":
-            lines.pop()
-        yield parse_example_with_generations(lines)
-
-
-def extract_definition_from_generation(gen: str):
-    try:
-        # Assume the prompt ends with a double-quote, so we expect the generated definition ends at another double-quote.
-        quote_index = gen.index('"')
-    except ValueError: # Cannot find the double-quote
-        pass # use the whole generation
-    else:
-        gen = gen[:quote_index] # use the quoted content
-    # for better formatting
-    gen = ' ' + gen.strip()
-    return gen
-
+used_output_fields = output_fields[:-1]
 
 prompt_qa_format_of_mode = {
     "example": "Please answer in a single uppercase letter: Which of the following is a better next example for the word '{word}', or they tie?\nA){}\nB){}\nC){}\nAnswer Choice:".format,
@@ -186,7 +102,7 @@ def get_case_name(winner_id: int, id_to_name: Mapping[int, str]):
 def print_statistics(results, id_to_name: Mapping[int, str], judges):
     for judge in judges:
         print(f"{judge}:")
-        for output_field in output_fields:
+        for output_field in used_output_fields:
             print(f"{output_field}:")
             winner_ids = [
                 winner_ids[judge]
@@ -203,7 +119,7 @@ def print_statistics(results, id_to_name: Mapping[int, str], judges):
         cnt_eq, cnt_oppo, tot = 0, 0, 0
         cnt_table = np.zeros((3, 3), dtype=int)
         for example_results in results:
-            for output_field in output_fields:
+            for output_field in used_output_fields:
                 for winner_ids in example_results[output_field]:
                     try:
                         winner_id_pair = tuple((winner_ids[judge] for judge in judge_pair))
@@ -332,7 +248,7 @@ if __name__ == "__main__":
                 results.append({})
             example_results = results[i_example]
 
-            for field in output_fields:
+            for field in used_output_fields:
                 if field not in example_results:
                     example_results[field] = []
                 field_results = example_results[field]
---
+++ read_generations.py
@@ -0,0 +1,88 @@
+from collections.abc import Iterable
+
+
+input_fields = [
+    "ground-truth word",
+    "ground-truth prefix",
+    "     decoded prefix",
+    "ground-truth suffix",
+]
+output_fields = [
+    "greedy outputs",
+    "sample with top-p=0.92 outputs",
+    "beam search outputs",
+]
+fields = input_fields + output_fields
+
+
+def parse_example_with_generations(
+        lines: Iterable[str],
+        fields=fields,
+        cont_prefix_pattern="cont. {}: ".format,
+):
+    example = {}
+    last_lines = []
+    for line in lines:
+        field, sep, rest = line.partition(":")
+        if sep and field in fields:
+            last_lines = [rest]
+            example[field.lstrip()] = last_lines
+        else:
+            last_lines.append(line)
+    for field, value_lines in example.items():
+        if field.endswith("outputs"):
+            assert len(value_lines) >= 1
+            assert value_lines[0] == "\n"
+            value_lines.pop(0)
+            value = []
+            n_cont = 0
+            cont_prefix = cont_prefix_pattern(n_cont)
+            while value_lines:
+                assert value_lines[0].startswith(cont_prefix)
+                cont = value_lines.pop(0).removeprefix(cont_prefix)
+                n_cont += 1
+                cont_prefix = cont_prefix_pattern(n_cont)
+                while value_lines and not value_lines[0].startswith(cont_prefix):
+                    cont += value_lines.pop(0)
+                cont = cont.removesuffix("\n")
+                value.append(cont)
+        else:
+            value = "".join(value_lines)
+            value = value.removeprefix(" ")
+            value = value.removesuffix("\n")
+        example[field] = value
+    return example
+
+
+def read_generations(file, example_first_line_pattern="Example #{}:\n".format):
+    n_example = 0
+    example_first_line = example_first_line_pattern(n_example)
+    line = file.readline()
+    while True:
+        while line and line != example_first_line:
+            line = file.readline()
+        if not line:  # EOF
+            break
+        line = file.readline()
+        n_example += 1
+        example_first_line = example_first_line_pattern(n_example)
+        lines = []
+        while line and line != example_first_line:
+            lines.append(line)
+            line = file.readline()
+        if lines and lines[-1] == "\n":
+            lines.pop()
+        yield parse_example_with_generations(lines)
+
+
+def extract_definition_from_generation(gen: str):
+    try:
+        # Assume the prompt ends with a double-quote, so we expect the generated definition ends at another double-quote.
+        quote_index = gen.index('"')
+    except ValueError: # Cannot find the double-quote
+        pass # use the whole generation
+    else:
+        gen = gen[:quote_index] # use the quoted content
+    # for better formatting
+    gen = ' ' + gen.strip()
+    return gen
